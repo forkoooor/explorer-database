5828e93d6c2504a98640252d4b016659274c96cd
fun <lljxx1@gmail.com>
2022-07-01 00:21:57 +0800

    update

--- tweet-bot.js
+++ tweet-bot.js
@@ -128,7 +128,7 @@ async function getNeedPostTweet() {
         } catch(e) {
           console.log('failed', e)
         }
-        await wait(60 * 1000 * 60);
+        await wait(60 * 1000 * 100);
       }
     } catch(e) {
       console.log('error', e)
